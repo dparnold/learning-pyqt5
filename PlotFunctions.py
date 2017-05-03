193c6f262ccb44e7ba52822a84a3f5044ad66604
dparnold <d.p.arnold@gmx.de>
2017-05-03 22:07:03 +0200

    Added some files and changed the plot to mainwindow

---
+++ PlotFunctions.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial 
+
+This program creates a statusbar.
+
+author: Jan Bodnar
+website: zetcode.com 
+last edited: January 2015
+"""
+
+import sys, os
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtWidgets import (QMainWindow, QWidget, QLabel, QLineEdit, QTextEdit, QGridLayout, QApplication, QPushButton, 
+    QHBoxLayout, QVBoxLayout, QApplication, qApp, QAction, QToolTip)
+
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+
+
+class MainWindow(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+        
+    def initUI(self):  
+
+ 
+
+        plotFunctionAction = QAction(QIcon('plotIcon.png'), 'Plot f(x)', self)        
+        plotFunctionAction.setShortcut('Ctrl+P')
+        plotFunctionAction.setStatusTip('Plot a function f(x)')
+        plotFunctionAction.triggered.connect(self.plotFunctionButtonClicked)     
+        
+        PlotDataAction = QAction(QIcon('sound.png'), '&Exit', self)        
+        PlotDataAction.setShortcut('Ctrl+Q')
+        PlotDataAction.setStatusTip('Exit application')
+        PlotDataAction.triggered.connect(self.plotDataButtonClicked)  
+             
+        """"Set the Toolbar """
+        self.toolbar = self.addToolBar('Main toolbar')
+        self.toolbar.addAction(plotFunctionAction)
+        self.toolbar.addAction(PlotDataAction)
+        self.toolbar.setIconSize(QSize(60, 40))
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+        fileMenu.addAction(plotFunctionAction)
+        settingsMenu = menubar.addMenu('Settings')
+        
+        self.statusBar().showMessage('Ready')
+        
+        """"Set the central Widget """
+        self.plotFunctionWidget = PlotFunctionWidget()
+        self.setCentralWidget(self.plotFunctionWidget)
+        
+        self.showMaximized()
+        self.setWindowTitle('Python Qt5 Plot')    
+        self.show()
+    #Key Events
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()   
+    def plotFunctionButtonClicked(self):
+        self.plotFunctionWidget = PlotFunctionWidget()
+        self.setCentralWidget(self.plotFunctionWidget)
+    def plotDataButtonClicked(self):
+        self.plotDataWidget = PlotDataWidget()
+        self.setCentralWidget(self.plotDataWidget)
+      
+class PlotFunctionWidget(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):
+        self.graphWidget = QWidget(self)
+        self.graph = PlotFunctionCanvas(self.graphWidget)
+        
+        self.textEdit = QTextEdit()
+
+        expression = QLabel('f(x)=')
+        self.expressionEdit = QLineEdit()
+        self.expressionEdit.setMinimumWidth(200);
+                
+        startvalue = QLabel('from x=:')
+        self.startvalueEdit = QLineEdit()
+        self.startvalueEdit.setMinimumWidth(200);
+
+        endvalue = QLabel('to x=:')
+        self.endvalueEdit = QLineEdit()
+        self.endvalueEdit.setMinimumWidth(200);
+
+
+        plotButton = QPushButton("plot")
+        plotButton.clicked.connect(self.plotButtonClicked) 
+
+
+        hbox = QHBoxLayout()
+        grid_menu = QGridLayout()
+        grid_menu.addWidget(expression, 0, 0)
+        grid_menu.addWidget(self.expressionEdit, 0, 1)
+        grid_menu.addWidget(startvalue, 1, 0)
+        grid_menu.addWidget(self.startvalueEdit, 1, 1)
+        grid_menu.addWidget(endvalue, 2, 0)
+        grid_menu.addWidget(self.endvalueEdit, 2, 1)
+        grid_menu.addWidget(plotButton, 3,1)
+                
+        hbox.addLayout(grid_menu)
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+        
+        
+        grid = QGridLayout()
+        grid.setColumnStretch(0, 1)
+        grid.setColumnStretch(1, 7)
+        grid.setSpacing(5)
+        
+        grid.addLayout(vbox, 0, 0)
+        
+        grid.addWidget(self.graph, 0, 1)
+        
+        
+        self.setLayout(grid) 
+        
+        self.showMaximized()
+        self.setWindowTitle('Python Qt5 Plot')    
+        self.show()
+        
+    def plotButtonClicked(self):
+        self.graph.update_figure(str(self.expressionEdit.text()),float(self.startvalueEdit.text()),float(self.endvalueEdit.text()))    
+
+class PlotDataWidget(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+    def initUI(self):  
+        
+        QToolTip.setFont(QFont('SansSerif', 10))
+        
+        self.setToolTip('This is a <b>QWidget</b> widget')
+        
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)     
+        self.showMaximized()
+        self.setWindowTitle('Python Qt5 Plot')    
+        self.show()
+class MplCanvas(FigureCanvasQTAgg):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+    def __init__(self, parent=None):
+        fig = Figure()
+        self.axes = fig.add_subplot(111)
+        self.compute_initial_figure()
+        FigureCanvasQTAgg.__init__(self, fig)
+        self.setParent(parent)
+        FigureCanvasQTAgg.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        FigureCanvasQTAgg.updateGeometry(self)
+    def compute_initial_figure(self):
+        pass
+class PlotFunctionCanvas(MplCanvas):
+    def compute_initial_figure(self):
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2*pi*t)
+        self.axes.plot(t,s)   
+        self.axes.grid()
+    def update_figure(self, expression, beginning, end):
+        x = arange(beginning,end,(end-beginning)/1000.0)
+        exec('y = ' +str(expression),locals(),globals())
+        self.axes.clear()
+        self.axes.plot(x,y,'r')
+        self.axes.grid()  
+        self.draw()          
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
--- qt5pythonplotting.py
+++ qt5pythonplotting.py
@@ -7,109 +7,120 @@ import random
 import matplotlib
 matplotlib.use('Qt5Agg')
 
+from PyQt5.QtCore import Qt
+
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QTextEdit, QGridLayout, QApplication, QPushButton, 
-    QHBoxLayout, QVBoxLayout)
+from PyQt5.QtWidgets import (QMainWindow, QWidget, QLabel, QLineEdit, QTextEdit, QGridLayout, QApplication, QPushButton, 
+	QHBoxLayout, QVBoxLayout, QApplication)
 
 from numpy import arange, sin, pi
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 
 
-class MyMplCanvas(FigureCanvas):
-    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+class MyMplCanvas(FigureCanvasQTAgg):
+	"""Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+	def __init__(self, parent=None):
+		fig = Figure()
+		self.axes = fig.add_subplot(111)
+		self.axes.hold(False) # Clear the Axis everytime plot() is called
+		self.compute_initial_figure()
+		FigureCanvasQTAgg.__init__(self, fig)
+		self.setParent(parent)
+		FigureCanvasQTAgg.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+		FigureCanvasQTAgg.updateGeometry(self)
+	def compute_initial_figure(self):
+		pass
 
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        # We want the axes cleared every time plot() is called
-        self.axes.hold(False)
-
-        self.compute_initial_figure()
-
-        #
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-
-        FigureCanvas.setSizePolicy(self,
-                                   QtWidgets.QSizePolicy.Expanding,
-                                   QtWidgets.QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-
-    def compute_initial_figure(self):
-        pass
-
-"""Simple canvas with a sine plot."""
+"""Simple canvas with a sine plot."""	
 class MyStaticMplCanvas(MyMplCanvas):
-    def compute_initial_figure(self):
-        t = arange(0.0, 3.0, 0.01)
-        s = sin(2*pi*t)
-        self.axes.plot(t,s)     
+	def compute_initial_figure(self):
+		t = arange(0.0, 3.0, 0.01)
+		s = sin(2*pi*t)
+		self.axes.plot(t,s)   
+		self.axes.grid()
+	def update_figure(self, expression, beginning, end):
+		x = arange(beginning,end,(end-beginning)/1000.0)
+		exec('y = ' +str(expression),locals(),globals())
+		self.axes.plot(x,y,'r')
+		self.axes.grid()  
+		self.draw()  
 
 class Example(QWidget):
-    
-    def __init__(self):
-        super().__init__()
-        
-        self.initUI()
-        
-        
-    def initUI(self):
-        
+	
+	def __init__(self):
+		super().__init__()
+		
+		self.initUI()
+		
+		
+	def initUI(self):
+		
 
-        expression = QLabel('f(x)=')
-        expressionEdit = QLineEdit()
-        expressionEdit.setMinimumWidth(200);
-                
-        startvalue = QLabel('from x=:')
-        startvalueEdit = QLineEdit()
-        startvalueEdit.setMinimumWidth(200);
+		expression = QLabel('f(x)=')
+		self.expressionEdit = QLineEdit()
+		self.expressionEdit.setMinimumWidth(200);
+				
+		startvalue = QLabel('from x=:')
+		self.startvalueEdit = QLineEdit()
+		self.startvalueEdit.setMinimumWidth(200);
 
-        endvalue = QLabel('to x=:')
-        endvalueEdit = QLineEdit()
-        endvalueEdit.setMinimumWidth(200);
-        
-        self.main_widget = QtWidgets.QWidget(self)
-        graph = MyStaticMplCanvas(self.main_widget)
+		endvalue = QLabel('to x=:')
+		self.endvalueEdit = QLineEdit()
+		self.endvalueEdit.setMinimumWidth(200);
+		
+		self.main_widget = QWidget(self)
+		self.graph = MyStaticMplCanvas(self.main_widget)
 
 
-        plotButton = QPushButton("plot")
+		plotButton = QPushButton("plot")
+		plotButton.clicked.connect(self.plotButtonClicked) 
 
-        hbox = QHBoxLayout()
-        grid_menu = QGridLayout()
-        grid_menu.addWidget(expression, 0, 0)
-        grid_menu.addWidget(expressionEdit, 0, 1)
-        grid_menu.addWidget(startvalue, 1, 0)
-        grid_menu.addWidget(startvalueEdit, 1, 1)
-        grid_menu.addWidget(endvalue, 2, 0)
-        grid_menu.addWidget(endvalueEdit, 2, 1)
-        grid_menu.addWidget(plotButton, 3,1)
-                
-        hbox.addLayout(grid_menu)
 
-        vbox = QVBoxLayout()
-        vbox.addLayout(hbox)
-        vbox.addStretch(1)
+		hbox = QHBoxLayout()
+		grid_menu = QGridLayout()
+		grid_menu.addWidget(expression, 0, 0)
+		grid_menu.addWidget(self.expressionEdit, 0, 1)
+		grid_menu.addWidget(startvalue, 1, 0)
+		grid_menu.addWidget(self.startvalueEdit, 1, 1)
+		grid_menu.addWidget(endvalue, 2, 0)
+		grid_menu.addWidget(self.endvalueEdit, 2, 1)
+		grid_menu.addWidget(plotButton, 3,1)
+				
+		hbox.addLayout(grid_menu)
+
+		vbox = QVBoxLayout()
+		vbox.addLayout(hbox)
+		vbox.addStretch(1)
+		
+		
+		grid = QGridLayout()
+		grid.setColumnStretch(0, 1)
+		grid.setColumnStretch(1, 7)
+		grid.setSpacing(5)
+		
+		grid.addLayout(vbox, 0, 0)
+		
+		grid.addWidget(self.graph, 0, 1)
+		#self.statusBar()
         
-        
-        grid = QGridLayout()
-        grid.setColumnStretch(0, 1)
-        grid.setColumnStretch(1, 7)
-        grid.setSpacing(5)
-        
-        grid.addLayout(vbox, 0, 0)
-        
-        grid.addWidget(graph, 0, 1)
-        
-        self.setLayout(grid) 
-        
-        self.showMaximized()
-        self.setWindowTitle('Python Qt5 Plot')    
-        self.show()
-        
-        
+		
+		self.setLayout(grid) 
+		
+		self.showMaximized()
+		self.setWindowTitle('Python Qt5 Plot')	
+		self.show()
+		
+	def plotButtonClicked(self):
+		self.graph.update_figure(str(self.expressionEdit.text()),float(self.startvalueEdit.text()),float(self.endvalueEdit.text()))	
+	#Key Events
+	def keyPressEvent(self, e):
+		if e.key() == Qt.Key_Space:
+			self.plotButtonClicked()
+		elif e.key() == Qt.Key_Escape:
+			self.close()
 if __name__ == '__main__':
-    
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+	
+	app = QApplication(sys.argv)
+	ex = Example()
+	sys.exit(app.exec_())
